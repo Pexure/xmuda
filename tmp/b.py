792bfb63e05ea31921de4144e70728754263733e
pxy <pexure@gmail.com>
2021-01-24 19:39:46 -0800

    m

--- tmp/b.py
+++ tmp/b.py
@@ -1,24 +1,27 @@
 from xmuda.data.nuscenes.nuscenes_dataloader import NuScenesSCN
 import numpy as np
+import os.path as osp
 
 preprocess_dir = "/home/xyyue/xiangyu/nuscenes_unzip/xmuda_lidarseg_preprocess"
 nuscenes_dir = "/home/xyyue/xiangyu/nuscenes_unzip"
-split = ('train_day',)
+split = ('train_usa',)
 # pselab_paths = ('/home/docker_user/workspace/outputs/xmuda/nuscenes/day_night/xmuda/pselab_data/train_night.npy',)
 dataset = NuScenesSCN(split=split,
                       preprocess_dir=preprocess_dir,
                       nuscenes_dir=nuscenes_dir,
+                      scale=1,
                       # pselab_paths=pselab_paths,
                       merge_classes=True,
                       use_image=True,
-                      noisy_rot=0.1,
-                      flip_x=0.5,
-                      rot_z=2 * np.pi,
-                      transl=True,
-                      fliplr=0.5,
-                      color_jitter=(0.4, 0.4, 0.4)
                       )
-for i in range(10):
+import open3d as o3d
+pcd = o3d.geometry.PointCloud()
+v3d = o3d.utility.Vector3dVector
+
+for i in range(5):
     data = dataset[i]
-    seg_label = data['seg_label']
-    print(i, len(seg_label))
+    seg_points = data['coords']
+    pcd.points = v3d(seg_points)  # seg_points=(N, 3)
+    o3d.io.write_point_cloud(osp.join('tmp/xmuda_train_usa', f'{i}.pcd'), pcd)
+    print(i, len(seg_points))
+    print()
